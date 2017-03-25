be02117c636109307f5c804ac113a6c74338cb8d
Martin Suarez-Viacava <kontrolakka@gmail.com>
2017-03-25 15:51:31 -0400

    Initial Commit

---
+++ ConsoleApp1/Fibonachi.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Fibonachi
+    {
+        private ulong count = 0;
+        private ulong fibonum = 0;
+        public ulong Fibonum { get { return fibonum;  } }
+        public Fibonachi()
+        {
+
+        }
+
+        public Fibonachi(ulong fibo)
+        {
+            RunFibonachi(fibo);
+        }
+        public ulong RunFibonachi(ulong fibo)
+        {
+            //if (++count % 100000000 == 0)
+                //Debug.WriteLine(count.ToString("N", Program.nfi));
+            if (fibo == 1)
+                return 1;
+            else if (fibo == 0)
+                return 0;
+            else
+                return (RunFibonachi(fibo - 1) + RunFibonachi(fibo - 2));
+        }
+
+        public void RunThreadedFibonachi(object ofibo)
+        {
+            ulong fibo = (ulong)ofibo;
+            //if (++count % 100000000 == 0)
+                //Debug.WriteLine(count.ToString("N", Program.nfi));
+            if (fibo == 1)
+            { 
+                fibonum++;
+                Console.WriteLine(fibonum);
+            }
+            else if (fibo > 1)
+            {
+                new Thread(RunThreadedFibonachi).Start(fibo - 1);
+                new Thread(RunThreadedFibonachi).Start(fibo - 2);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/RestartProgramming.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static NumberFormatInfo nfi = new CultureInfo("en-US").NumberFormat;
+        static void Main(string[] args)
+        {
+            nfi.NumberDecimalDigits = 0;
+
+            Fibonachi fibo = new Fibonachi();
+            fibo.RunFibonachi(20);
+            //for (int i = 0; i <= 50; i++)
+                //Console.WriteLine("Fibonachi {0}: {1}", i, fibo.RunFibonachi(i).ToString("N", nfi));
+            Console.WriteLine("End");
+        }
+    }
+}
